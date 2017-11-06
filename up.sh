891238c6a77fc8369d730ea164ce4db831a5dcb7
Carlos Robles <carlos@estudiocarlosrobles.com>
2017-11-06 23:44:18 +0400

    built

--- up.sh
+++ up.sh
@@ -3,5 +3,5 @@
 npm run build
 docker rm -f swagger
 docker build -t sam/swagger:latest .
-docker run --name swagger -p 8035:8080   -v $(pwd)/wiki:/wiki -d sam/swagger:latest
+docker run --name swagger -p 8035:8080  -v $(pwd)/wiki:/wiki -d sam/swagger:latest
 
